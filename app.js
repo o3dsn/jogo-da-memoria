86495021f2057eb742c213f80b50d9f284a5d644
Edson Junior <edson_a@outlook.com>
2021-01-28 09:10:44 -0300

    segunda aula

--- app.js
+++ app.js
@@ -14,10 +14,13 @@ const board = document.querySelector(".table__board");
 
 let pairs = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 
+let selectedCards = [];
+
 pairs.forEach(pair => {
     const card = document.createElement("div");
 
     card.className = "table__board__card";
+    card.dataset.value = pair;
     card.innerText = pair;
 
     card.addEventListener("click", itIsAMatch);
@@ -25,9 +28,38 @@ pairs.forEach(pair => {
     board.append(card);
 });
 
-let value1
-let value2
+
 
 function itIsAMatch(value) {
-    console.log(value.target)
+    const clickedCard = value.target;
+
+    if(clickedCard.classList.contains('open')) {
+        return;
+    }
+
+    clickedCard.classList.add('open');
+
+    selectedCards.push(clickedCard);
+
+    if(selectedCards.length > 1) {
+        const [firstCard, secondCard] = [...selectedCards];
+
+        if(firstCard.dataset.value === secondCard.dataset.value) {
+            console.log("Formou par")
+
+            selectedCards = [];
+        } else {
+           
+            selectedCards = [];
+
+            setTimeout(() => {
+                firstCard.classList.remove('open');
+                secondCard.classList.remove('open');
+            }, 800);
+
+            // milisegundos 1000ms = 1s
+        }
+    }
+
+    
 }
